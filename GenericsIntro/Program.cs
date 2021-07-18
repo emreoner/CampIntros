5259b2096afcce8a174a8145e20e0fe78e6969cf
Emre ONER <oneremre71@gmail.com>
2021-07-18 23:22:35 +0300

    Add project files.

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<string> myList = new MyList<string>();
+            myList.Add("Emre");
+            Console.WriteLine(myList.Length);
+            myList.Add("Ebrar Alya");
+            Console.WriteLine(myList.Length);
+            foreach (var item in myList.Items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
